80a28fef0b8e86f3da1672fd76fd00569515a512
Nadir Eliel Jaramillo Rosas <nadirjaramillorosas@hotmail.com>
2022-01-23 19:44:15 -0300

    setting props and state on Square

--- src/index.js
+++ src/index.js
@@ -3,19 +3,34 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+
     render() {
-      return (
-        <button className="square">
-          {/* TODO */}
-        </button>
-      );
+        return (
+            <button className="square"
+                onClick={() => this.props.onClick()}
+            >
+                {this.props.value}
+            </button>
+        );
     }
   }
   
   class Board extends React.Component {
-    renderSquare(i) {
-      return <Square />;
+    
+    constructor(props){
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+        }
     }
+
+      renderSquare(i) {
+          return (
+              <Square
+                  value={this.state.squares[i]}
+                  onClick={() => this.handleClick(i)} />
+          );
+      }
   
     render() {
       const status = 'Next player: X';
